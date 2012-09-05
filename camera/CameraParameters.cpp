117b17b2a3fe2ad544b3f06d9b15154f59f69a21
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-09-05 11:43:48 +0100

    CameraParameters: Underp non-qcom hardware
    Move setPostviewSize to QCOM_HARDWARE
    
    Change-Id: I830d1385be0e784f9fcb6dc40f014bea36b4b65b

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -593,11 +593,6 @@ static void parseSizesList(const char *sizesStr, Vector<Size> &sizes)
     }
 }
 
-void CameraParameters::setPostviewSize(int width, int height)
-{
-    // dummy
-}
-
 void CameraParameters::setPreviewSize(int width, int height)
 {
     char str[32];
@@ -634,6 +629,11 @@ void CameraParameters::getSupportedPreviewSizes(Vector<Size> &sizes) const
 }
 
 #ifdef QCOM_HARDWARE
+void CameraParameters::setPostviewSize(int width, int height)
+{
+    // dummy
+}
+
 void CameraParameters::getSupportedHfrSizes(Vector<Size> &sizes) const
 {
     const char *hfrSizesStr = get(KEY_SUPPORTED_HFR_SIZES);
